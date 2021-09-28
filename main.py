6d3a3e3bab456022ffba71cf29fcf9113cf9af3b
franck-armand <52790721+franck225-coder@users.noreply.github.com>
2021-09-28 17:45:26 +0800

    Exploratory data analysis

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ np.random.seed(seed=1234)
 #load data
 # Read from CSV to Pandas DataFrame
 url ="https://raw.githubusercontent.com/GokuMohandas/MadeWithML/main/datasets/titanic.csv"
-df = pd.read_csv(url, header = 0)
+df = pd.read_csv(url, header=0)
 df.head()
+
+# Exploratory data analysis
+import matplotlib.pyplot as plt
+
+# We can aslo extract some standard details by using the described functions
+df.describe()
+
+# Correlation matrix
